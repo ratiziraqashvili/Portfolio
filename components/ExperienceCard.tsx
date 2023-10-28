d6a4c51ce8ff22d86f69346bf2ebb1b840c391f2
Rati <rziraqashvili@gmail.com>
2023-10-28 17:17:08 +0400

    Fixing some bugs

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -7,7 +7,7 @@ type Props = {
   experience: Experience;
 };
 
-function ExperienceCard({experience}: Props) {
+function ExperienceCard({ experience }: Props) {
   return (
     <article className="flex pb-36 flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[600px] snap-center bg-[#292929] mt-30 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden  ">
       <motion.img
@@ -34,16 +34,23 @@ function ExperienceCard({experience}: Props) {
         <h4 className="text-4xl font-light">{experience?.jobTitle}</h4>
         <p className="font-bold text-2xl mt-1">{experience?.company}</p>
         <div className="flex space-x-2 my-2">
-          {experience.technologies.map(technology => (
-            <img key={technology._id} className='h-10 w-10 rounded-full' src={urlFor(technology.image).url()} alt="" />
+          {experience.technologies.map((technology) => (
+            <img
+              key={technology._id}
+              className="h-10 w-10 rounded-full"
+              src={urlFor(technology.image).url()}
+              alt=""
+            />
           ))}
         </div>
-        <p className="uppercase py-5 text-gray-300">{experience?.dateStarted}</p>
+        <p className="uppercase py-5 text-gray-300">
+          {experience?.dateStarted}
+        </p>
 
         <ul className="list-none max-h-96 space-y-4 ml-5 text-lg h-auto ">
-            {experience?.points.map((point, i) => (
-              <li key={i}>{point}</li>
-            ))}
+          {experience &&
+            experience.points &&
+            experience.points.map((point, i) => <li key={i}>{point}</li>)}
         </ul>
       </div>
     </article>
